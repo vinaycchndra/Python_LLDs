dbcb7f6380227e1dbced7762f1dd98df4c67686c
Vinay <Vinay@example.com>
2024-10-05 23:12:03 +0530

    completed the sequential huffman based file compressor module

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python_LLDs
 Creating a repository for the implementation of the low level implementation of the algorithms and data structure of the standard problems.
+    1. The huffman based file compressor is written for text files containing ascii and extended ascii characters from range 0-255. A sample text  file 'sample.txt' is given as an input. The compressed file itself contanins the keys for decoding the compressed text. So any text file which is compressed to .bin file using this module can be decompressed to text file using this same module. 
+
--- huffman_compression/main.py
+++ huffman_compression/main.py
@@ -1,24 +1,11 @@
 import random
-from huffman_encoder import HuffmanEncoder
-from huffman_decoder import HuffmanDecoder
+from file_handler import FileHandler
 
-input_text = []
-for i in range(100, 120):
-    count_char  = random.randint(400000, 550000)
-    input_text.append(chr(i)*count_char)
-input_text = "".join(input_text)
-input_text = ''.join(random.sample(input_text,len(input_text)))
+input_file_path = "./sample.txt"
+filhandler = FileHandler(compress = True, input_file_path=input_file_path)
+filhandler.process()
 
-encoder = HuffmanEncoder(input_text)
+output_file_path = "./sample_compressed.bin"
 
-# Calls encoder
-encoder.encode()
-encode_input_text = encoder.get_encoded_bytes()
-decode_map = encoder.get_decode_key_map()
-
-# Calls decoder
-obj = HuffmanDecoder(encoder.get_encoded_bytes())
-output_text = obj.decode()
-
-# Compression ratio
-print("compression ratio:::", len(encode_input_text)/len(input_text))
+filhandler = FileHandler(compress = False, input_file_path=output_file_path)
+filhandler.process()
