2470d73e6400958c16bcb2fd772a616862e422b7
Vinay <Vinay@example.com>
2024-10-14 20:55:04 +0530

    token bucket based rate limiter complete

--- Rate_Limiter/RateLimit.py
+++ Rate_Limiter/RateLimit.py
@@ -7,29 +7,34 @@ class RateLimiter:
         
         # permitted rate can not be zero or None
         assert self.permitted_rate
+
         # only one parameter should be true
         assert perMinute+perHour+perSecond == 1
 
         # Fixing a minimum window limit for the normal system to handle bulk refilling of the clients tokens 
         # for example in a token bucket class: If running a loop to refilling the buckets of the users with a single for loop. if there are more clients accessing
-        # then a loop may take more 100 milli second to complete the refilling of all the users.  
+        # then a loop may take more 100 milli second to complete the refilling of all the users which may not allow that exact rate of filling of 1 token for all clients
+        # every 100 mili second.
+          
         if perSecond:
             if self.permitted_rate> 10:
-                raise Exception("Per second config does not allow more than the 10 requests per second to avoid complexity")
+                raise Exception("Per second config does not allow more than the 10 requests per second to fill min number of client's bucket.")
             self.time_limit = 1/self.permitted_rate
 
         if perMinute:
             if self.permitted_rate> 600:
-                raise Exception("Per minute config does not allow more than the 600 requests per minute to avoid complexity")
+                raise Exception("Per minute config does not allow more than the 600 requests per minute to fill min number of client's bucket.")
             self.time_limit = 60/self.permitted_rate
         
         if perHour:
             if self.permitted_rate> 24000:
-                raise Exception("Per hour config does not allow more than the 24000 requests per hour to avoid complexity")
+                raise Exception("Per hour config does not allow more than the 24000 requests per hour to fill min number of client's bucket.")
             self.time_limit = 2400/self.permitted_rate
 
         if self.algo ==  "token_bucket":
-            self.algo = TokenBucket(time_limit = self.time_limit)
+            self.__rate_limiter = TokenBucket(timelimit = self.time_limit)
         else:
-            raise Exception("Please provide the valid rate limiting algorithm to use.")
+            raise Exception("Please provide the valid rate limiting algorithm from 'token_bucket' or ' '.")
         
+    def get_rate_limiter(self):
+        return self.__rate_limiter
---
+++ Rate_Limiter/main.py
@@ -0,0 +1,47 @@
+import asyncio
+from RateLimit import RateLimiter 
+import time
+
+
+# mimicking the request of the user from a maximum user base of 5000 clients and maximum rate limit possible is 10 and every user is given 
+# equal share of requests. 
+async def coming_requsts(rl, rate_limiter_loop):
+    t1 = time.time()
+    a = 1
+    b = 5000 # max users
+    approved = {i:0 for i in range(a, b+1)}
+    total_request = {i:0 for i in range(a, b+1)}
+    total_incoming = 10000000
+    for k in range(1, total_incoming+1):
+        user = k%b
+        if user == 0:
+            user = b
+        total_request[user] += 1
+        await asyncio.sleep(0.000000000001)
+        approve = await rl.approve_request(user)
+        if approve:
+            approved[user] = approved[user]+1
+        
+    
+    total_time = time.time()-t1
+    
+    for i in range(a, b+1):
+        print(f"user : {i}--> total_requests: {total_request.get(i)}--> Approved Requests: {approved.get(i)}--> in time: {total_time}--> Approved rate: {approved.get(i)/total_time}")
+    
+    total_approved = 0
+    total_requested = 0
+    
+    for i in range(a, b+1):    
+        total_approved += approved[i]
+        total_requested += total_request[i]
+    print(f"Total processed requests: {total_approved} out of {total_requested} requests.")
+    rate_limiter_loop.cancel()
+
+async def main():
+    rl_config = RateLimiter("token_bucket", 2, perSecond=True)
+    rl =  rl_config.get_rate_limiter()
+    task1 = asyncio.create_task(rl.start())
+    task2 = asyncio.create_task(coming_requsts(rl, task1))
+    await asyncio.gather(task1, task2)
+
+asyncio.run(main())
