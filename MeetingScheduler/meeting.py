afee040ac205975a3fc865523be2c7e2cb6c84a6
Vinay <Vinay@example.com>
2024-12-19 09:36:22 +0530

    completed meeting class

--- MeetingScheduler/meeting.py
+++ MeetingScheduler/meeting.py
@@ -1,3 +1,51 @@
+from account.user import User
+from datetime import datetime
 
 class Meeting: 
-    pass
+    def __init__(self, meetingId: str): 
+        self._meetingId = meetingId
+        self._meetingRoomId = None
+        self._host = None
+        self._invitees = []
+        self._description = None
+        self._startTime = None
+        self._endTime = None
+
+    def getMeetingId(self) -> str:
+        return self._meetingId
+    
+    def getMeetingRoomId(self) -> str: 
+        return self._meetingRoomId
+    
+    def getHost(self) -> User:
+        return self._host
+    
+    def getListOfInvitees(self) -> list[User]: 
+        return self._invitees
+    
+    def getDescription(self) -> str: 
+        return self._description
+    
+    def setMeetingRoomId(self, meetingRoomId: str) -> "Meeting": 
+        self._meetingRoomId  = meetingRoomId
+        return self
+    
+    def setHost(self, host: User) -> "Meeting": 
+        self._host = host
+        return self
+    
+    def addInvitee(self, user: User) -> "Meeting": 
+        for user_ in self._invitees:
+            if user_ == user: 
+                return self
+        self._invitees.append(user)
+        return self
+    
+    def setStartTime(self, startTime: datetime)->"Meeting": 
+        self._startTime = startTime
+        return self
+    
+    def setEndTime(self, endTime: datetime)->"Meeting": 
+        self._endTime = endTime
+        return self
+    
